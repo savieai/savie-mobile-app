9647788fdeaaa9ba621f577a6ff1e05e6090b3ba
Emil Khabibullin <emileyray23@icloud.com>
2024-06-22 21:11:48 +0300

    fix: keyboard dismiss behaviour

--- lib/presentation/page/chat/widget/message_list_view.dart
+++ lib/presentation/page/chat/widget/message_list_view.dart
@@ -1,4 +1,3 @@
-import 'package:cupertino_interactive_keyboard/cupertino_interactive_keyboard.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
@@ -10,38 +9,37 @@ class MessageListView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return CupertinoInteractiveKeyboard(
-      child: CustomScrollView(
-        reverse: true,
-        slivers: <Widget>[
-          ...<Widget>[
-            const SliverToBoxAdapter(child: SizedBox(height: 16)),
-            const WelcomeMessageListView(),
-            const SliverToBoxAdapter(child: SizedBox(height: 16)),
-            BlocBuilder<ChatCubit, ChatState>(
-              builder: (BuildContext context, ChatState state) {
-                final int length = state.messages.length;
+    return CustomScrollView(
+      keyboardDismissBehavior: ScrollViewKeyboardDismissBehavior.onDrag,
+      reverse: true,
+      slivers: <Widget>[
+        ...<Widget>[
+          const SliverToBoxAdapter(child: SizedBox(height: 16)),
+          const WelcomeMessageListView(),
+          const SliverToBoxAdapter(child: SizedBox(height: 16)),
+          BlocBuilder<ChatCubit, ChatState>(
+            builder: (BuildContext context, ChatState state) {
+              final int length = state.messages.length;
 
-                return SliverList.separated(
-                  itemBuilder: (BuildContext context, int index) {
-                    return MessageView(
-                      message: state.messages[length - index - 1],
-                    );
-                  },
-                  separatorBuilder: (_, __) => const SizedBox(height: 12),
-                  itemCount: length,
-                );
-              },
-            ),
-            const SliverToBoxAdapter(child: SizedBox(height: 32)),
-          ].reversed.map(
-                (Widget w) => SliverPadding(
-                  padding: const EdgeInsets.symmetric(horizontal: 12),
-                  sliver: w,
-                ),
+              return SliverList.separated(
+                itemBuilder: (BuildContext context, int index) {
+                  return MessageView(
+                    message: state.messages[length - index - 1],
+                  );
+                },
+                separatorBuilder: (_, __) => const SizedBox(height: 12),
+                itemCount: length,
+              );
+            },
+          ),
+          const SliverToBoxAdapter(child: SizedBox(height: 32)),
+        ].reversed.map(
+              (Widget w) => SliverPadding(
+                padding: const EdgeInsets.symmetric(horizontal: 12),
+                sliver: w,
               ),
-        ],
-      ),
+            ),
+      ],
     );
   }
 }
